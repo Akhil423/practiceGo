3afeec3ef94dc59875dd62ac4cfe627be76f214a
akhil varma <akhilvarma423@gmail.com>
2022-01-23 12:50:55 +0530

    check declare and initialize variables

---
+++ practice/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var aString string = "This is my go practice"
+
+	fmt.Println(aString)
+	fmt.Printf("the variable type is %T\n", aString)
+	var anInteger int = 24
+	fmt.Println(anInteger)
+
+	var defaultVal int
+
+	fmt.Println(defaultVal)
+
+	var newString = "definition without type"
+	fmt.Println(newString)
+	fmt.Printf("the variable type is %T\n", newString)
+
+	newString2 := "colon declarion" //only accessible inside the class
+	fmt.Println(newString2)
+	fmt.Printf("the variable type is %T\n", newString2)
+
+}
